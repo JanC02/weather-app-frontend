6e93a0571d140c34dc488e9a2b51f8741ac50974
Jan Chrobak <jan.chrobak02@outlook.com>
2025-08-19 13:21:19 +0200

    add min temperature for daily weather

--- src/components/WeatherDashboard/ForecastItem.tsx
+++ src/components/WeatherDashboard/ForecastItem.tsx
@@ -2,18 +2,20 @@ import { getIcon } from "../../utils/getIcon.ts";
 
 type ForecastItemProps = {
     dayName: string;
-    temperature: number;
+    minTemp: number;
+    maxTemp: number;
     weatherCode: number;
 };
 
-export default function ForecastItem({ dayName, temperature, weatherCode }: ForecastItemProps) {
+export default function ForecastItem({ dayName, minTemp, maxTemp, weatherCode }: ForecastItemProps) {
     const Icon = getIcon(weatherCode);
 
     return <li className="flex items-center p-3 bg-gray-50 rounded-lg">
         <span className="font-bold">{dayName}</span>
         <div className="ml-auto flex items-center gap-3">
-            <Icon />
-            <span className="font-bold w-14 text-right">{temperature}°C</span>
+            <Icon className="w-8 h-8" />
+            <span className="font-bold text-right">{maxTemp}°C</span>
+            <span className="text-right text-gray-500">{minTemp}°C</span>
         </div>
     </li>
 }
--- src/contexts/WeatherContext.tsx
+++ src/contexts/WeatherContext.tsx
@@ -2,13 +2,13 @@ import { createContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
 import { WeatherService } from "../services/WeatherService";
 import { getWeatherDescription } from "../utils/weatherUtils";
-import { type WeatherDataType } from "../types.ts";
+import { type WeatherDataType, type DailyWeatherType } from "../types.ts";
 import { ZodError } from "zod";
 import { dateParser } from "../utils/dateParser.ts";
 
 type ResultWeatherType = WeatherDataType & {
     city: string;
-    dailyWeather: { dayName: string, temperature: number, weatherCode: number }[]
+    dailyWeather: DailyWeatherType[];
     description?: string;
 };
 
@@ -27,15 +27,18 @@ export default function WeatherContextProvider({ children }: { children: ReactNo
             const data = await WeatherService.getInstance().getWeather(latitude, longitude, city);
             const description = getWeatherDescription(data.current.weather_code);
 
-            const dailyWeather = data.daily.time.map((time, index) => {
+            console.log(data);
+
+            const dailyWeather: DailyWeatherType[] = data.daily.time.map((time, index) => {
                 let dayName = dateParser(time)[0];
                 if (index === 0) {
                     dayName += ' (dzisiaj)';
                 }
-                const temperature = data.daily.temperature_2m_max[index];
+                const minTemp = Math.round(data.daily.temperature_2m_min[index]);
+                const maxTemp = Math.round(data.daily.temperature_2m_max[index]);
                 const weatherCode = data.daily.weather_code[index];
 
-                return { dayName, temperature, weatherCode };
+                return { dayName, minTemp, maxTemp, weatherCode };
             });
 
             const newWeatherData: ResultWeatherType = {
