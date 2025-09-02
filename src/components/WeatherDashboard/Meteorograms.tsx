963912f4c1b759b40a7a91ebab09948ae0dcf8b9
Jan Chrobak <jan.chrobak02@outlook.com>
2025-09-02 20:14:07 +0200

    Fix existence of space in meteorograms container

--- src/components/WeatherDashboard/Meteorograms.tsx
+++ src/components/WeatherDashboard/Meteorograms.tsx
@@ -10,8 +10,8 @@ type MeteorogramProps = {
 export default function Meteorograms({ hourlyWeather }: MeteorogramProps) {
     return (
         <DashboardItem className='h-full flex flex-col grow'>
-            <h3 className="font-bold text-lg mb-2">Meteorogramy</h3>
-            <div className="rounded-lg overflow-x-auto overflow-y-hidden">
+            <h3 className="font-bold text-lg mb-2 shrink-0">Meteorogramy</h3>
+            <div className="rounded-lg overflow-x-auto overflow-y-hidden grow">
                 <div className="h-full">
                     <TemperatureMeteorogram hourlyWeather={hourlyWeather} />
                     <PrecipitationMeteorogram hourlyWeather={hourlyWeather} />
