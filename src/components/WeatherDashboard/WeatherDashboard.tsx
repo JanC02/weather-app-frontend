43d3e392fbf62fa5edaf6acc88752287b8933bd6
Jan Chrobak <jan.chrobak02@outlook.com>
2025-09-04 20:57:37 +0200

    feat: Move WeatherState type to types.ts, add DashboardStatus type. Improve UX by displaying spinner when LOADING, display error message when ERROR

--- src/components/WeatherDashboard/WeatherDashboard.tsx
+++ src/components/WeatherDashboard/WeatherDashboard.tsx
@@ -5,34 +5,66 @@ import { useWeather } from '../../hooks/useWeather';
 import CurrentWeather from './CurrentWeather';
 import Pressure from './Pressure';
 import Meteorograms from './Meteorograms.tsx';
+import type { WeatherState, DashboardStatus } from '../../types';
+import Spinner from '../Spinner';
 
-export default function WeatherDashboard() {
-    const { weatherData } = useWeather();
-
+const renderContent = (
+    weatherData: WeatherState | null, 
+    status: DashboardStatus
+) => {
     return (
         <div className='w-full max-w-6xl mx-auto p-4 grid grid-cols-1 lg:grid-cols-3 gap-8 items-start'>
 
             <div className="lg:col-span-1 flex flex-col gap-8">
                 <SearchBar/>
-                { weatherData && <>
+                { (weatherData && status === 'OK') && <>
                         <CurrentWeather temperature={weatherData.current.temperature} city={weatherData.city} description={weatherData.current.description} weatherCode={weatherData.current.weatherCode} isDay={weatherData.current.isDay} />
                         <DailyForecast dailyWeather={weatherData?.daily} />
                     </>
                 }
             </div>
 
-            { !weatherData && <div className="h-20 flex items-center justify-center lg:col-span-2 bg-white p-6 rounded-lg shadow-sm text-center">
-                <p>Wyszukaj miasto aby zobaczyć pogodę.</p>
-            </div> }
+            { status === 'LOADING' && (
+                <div className='lg:col-span-2 h-full bg-white rounded-lg shadow-sm flex items-center justify-center'>
+                    <Spinner />
+                </div> 
+            )}
 
-            { weatherData && <div className="lg:col-span-2 flex flex-col gap-8 h-full">
+            { (!weatherData && status === 'OK') && (
+                <div className="h-20 flex items-center justify-center lg:col-span-2 bg-white p-6 rounded-lg shadow-sm text-center">
+                    <p>Wyszukaj miasto aby zobaczyć pogodę.</p>
+                </div>
+            )}
+
+            { (weatherData && status === 'OK') && (
+                <div className="lg:col-span-2 flex flex-col gap-8 h-full">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
                         <Pressure pressure={weatherData.current.pressure}/>
                         <Humidity humidity={weatherData.current.humidity}/>
                     </div>
                     <Meteorograms hourlyWeather={weatherData.hourly} />
                 </div>
-            }
+            )}
         </div>
-    );
+    )
+};
+
+const renderError = () => {
+    return (
+        <div>
+            Przepraszamy, wystąpił błąd. Prosimy spróbować później.
+        </div>
+    )
+}
+
+export default function WeatherDashboard() {
+    const { weatherData, currentStatus } = useWeather();
+
+    switch(currentStatus) {
+        case 'LOADING':
+        case 'OK':
+            return renderContent(weatherData, currentStatus)
+        case 'ERROR':
+            return renderError()
+    }
 }
--- src/contexts/WeatherContext.tsx
+++ src/contexts/WeatherContext.tsx
@@ -2,35 +2,24 @@ import { createContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
 import { WeatherService } from "../services/WeatherService";
 import { getWeatherDescription } from "../utils/weatherUtils";
-import type { DailyWeatherType, HourlyDataType } from "../types.ts";
+import type { DailyWeatherType, HourlyDataType, WeatherState, DashboardStatus } from "../types.ts";
 import { ZodError } from "zod";
 import { dateParser } from "../utils/dateParser.ts";
-
-type WeatherState = {
-    city: string;
-    current: {
-        temperature: number;
-        weatherCode: number;
-        pressure: number;
-        humidity: number;
-        isDay: 0 | 1;
-        description?: string;
-    };
-    daily: DailyWeatherType[];
-    hourly: HourlyDataType[];
-};
 
 type WeatherContextType = {
     weatherData: WeatherState | null;
     fetchWeather: (latitude: number, longitude: number, city: string) => Promise<void>;
+    currentStatus: DashboardStatus;
 }
 
 export const WeatherContext = createContext<WeatherContextType | null>(null);
 
 export default function WeatherContextProvider({ children }: { children: ReactNode }) {
     const [weatherData, setWeatherData] = useState<WeatherState| null>(null);
+    const [currentStatus, setCurrentStatus] = useState<DashboardStatus>('OK');
 
     const fetchWeather = async (latitude: number, longitude: number, city: string) => {
+        setCurrentStatus('LOADING');
         try {
             const data = await WeatherService.getInstance().getWeather(latitude, longitude, city);
             const description = getWeatherDescription(data.current.weather_code);
@@ -74,8 +63,10 @@ export default function WeatherContextProvider({ children }: { children: ReactNo
                 newWeatherData.current.description = description;
             }
 
+            setCurrentStatus('OK');
             setWeatherData(newWeatherData);
         } catch (error) {
+            setCurrentStatus('ERROR');
             if (error instanceof ZodError) {
                 console.error('An error has occuered during fetching weather data: ', error.message, error.issues, error.stack);
             } else {
@@ -93,7 +84,8 @@ export default function WeatherContextProvider({ children }: { children: ReactNo
 
     const value = {
         weatherData,
-        fetchWeather
+        fetchWeather,
+        currentStatus
     };
 
     return (
