e7746d9f50795546b94c45c967a5bfe36a06ccea
Jan Chrobak <jan.chrobak02@outlook.com>
2025-09-16 21:48:09 +0200

    feat: add ApexChart attribution to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,31 @@
 export default function Footer() {
     return (
         <footer className='bg-transparent flex justify-center p-4 text-xs'>
-            <p className='flex items-center gap-'>
-                Weather and geocoding data by:
-                <a
-                    href="https://open-meteo.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="ml-1 underline hover:text-sky-800 transition-colors duration-150"
-                >
-                    Open-Meteo.com
-                </a>
+            <p className='flex items-center justify-center flex-wrap text-center'>
+                <span>
+                    Weather and geocoding data by:
+                    <a
+                        href="https://open-meteo.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="ml-1 underline hover:text-sky-800 transition-colors duration-150"
+                    >
+                        Open-Meteo.com
+                    </a>
+                </span>
+                <span className="mx-2">|</span>
+                <span>
+                    Charts by:
+                    <a
+                        href="https://apexcharts.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="ml-1 underline hover:text-sky-800 transition-colors duration-150"
+                    >
+                        ApexCharts.js
+                    </a>
+                </span>
             </p>
-
         </footer>
     )
 }
